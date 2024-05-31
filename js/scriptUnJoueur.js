5806c58f4c5fe4b6977674593f820dd5abf1a337
Maxime972 <67092377+Maxime972@users.noreply.github.com>
2024-05-31 17:38:01 +0200

    Ajout de la fonction newGame() et activer(id)

--- js/scriptUnJoueur.js
+++ js/scriptUnJoueur.js
@@ -1,48 +1,59 @@
-// Valeur current par défaut
+// Valeur current à zéro
 document.getElementById("scoreRound").innerHTML = 0;
 //console.log(document.getElementById("scoreRound").textContent);
 
-document.getElementById("globaleScore").innerHTML = 99;
+// Valeur globale à zéro
+document.getElementById("globaleScore").innerHTML = 0;
 
+// id des images des 6 faces du dés
 var dices = ["one", "two", "three", "four", "five", "six"];
+
 var visibleId = null;
 
 // La fonction permet d'afficher le dés
 function showDice(dice) {
-    if (visibleId !== dice) {
-      visibleId = dice;
-  
-      console.log("visible = " + visibleId);
-    }
-  
-    hide();
-  
-    // La fonction permet de cacher le dés
-    function hide() {
-      var div, i, dice;
-  
-      for (i = 0; i < dices.length; i++) {
-        dice = dices[i];
-        div = document.getElementById(dice);
-  
-        if (visibleId === dice){
-            div.style.display = "block";
-        } else {
-            div.style.display = "none";
-        }
-      }
-    }
+  if (visibleId !== dice) {
+    visibleId = dice;
+
+    console.log("visible = " + visibleId);
   }
 
-  // La méthode désactive un bouton
-function desactiver(id){
-  var node = document.getElementById(id);
-  var a = document.createAttribute("disabled");
-  a.value = "disabled";
-  node.setAttributeNode(a);
-  console.log(node.getAttribute("disabled"));
+  hide();
 }
 
+// La fonction permet de cacher le dés
+function hide() {
+  var div, i, dice;
+
+  for (i = 0; i < dices.length; i++) {
+    dice = dices[i];
+    div = document.getElementById(dice);
+
+    if (visibleId === dice) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
+
+// La fonction désactive un bouton
+function desactiver(id) {
+  var node = document.getElementById(id);
+  var attribut = document.createAttribute("disabled");
+  attribut.value = "disabled";
+  node.setAttributeNode(attribut);
+  //console.log(node.getAttribute("disabled"));
+}
+
+// La fonction active un bouton
+function activer(id) {
+  var node = document.getElementById(id);
+  node.removeAttribute("disabled");
+  //console.log(node.getAttribute("disabled"));
+}
+
+// La fonction permet de lancer le dés
 function rollDice() {
   var scoreRound = Number.parseInt(
     document.getElementById("scoreRound").textContent
@@ -54,52 +65,51 @@ function rollDice() {
   var des;
   switch (dice) {
     case 1:
-        des = "one"
-        break;
+      des = "one";
+      break;
 
-        case 2:
-        des = "two"
-        break;
+    case 2:
+      des = "two";
+      break;
 
-        case 3:
-        des = "three"
-        break;
+    case 3:
+      des = "three";
+      break;
 
-        case 4:
-        des = "four"
-        break;
+    case 4:
+      des = "four";
+      break;
 
-        case 5:
-        des = "five"
-        break;
+    case 5:
+      des = "five";
+      break;
 
-        case 6:
-        des = "six"
-        break;
-  
+    case 6:
+      des = "six";
+      break;
+
     default:
-        break;
+      break;
   }
 
   showDice(des);
-
 
   if (dice == 1) {
     document.getElementById("scoreRound").innerHTML = 0;
   } else {
     document.getElementById("scoreRound").innerHTML = scoreRound + dice;
   }
-  
-  //console.log(dice);
-
-  return dice;
 }
 
 // La fonction permet d'ajout le score temporaire au score glogale
-function hold(){
-  var globale = Number.parseInt(document.getElementById("globaleScore").textContent);
+function hold() {
+  var globale = Number.parseInt(
+    document.getElementById("globaleScore").textContent
+  );
 
-  var current = Number.parseInt(document.getElementById("scoreRound").textContent);
+  var current = Number.parseInt(
+    document.getElementById("scoreRound").textContent
+  );
 
   var globale = globale + current;
 
@@ -112,6 +122,18 @@ function hold(){
 
     desactiver("rollDice");
     desactiver("hold");
-   
   }
+}
+
+// Relance une nouvelle partie et réactiver les boutons
+function newGame() {
+  // Valeur current à zéro
+  document.getElementById("scoreRound").innerHTML = 0;
+  //console.log(document.getElementById("scoreRound").textContent);
+
+  // Valeur globale à zéro
+  document.getElementById("globaleScore").innerHTML = 0;
+
+  activer("rollDice");
+  activer("hold");
 }
